24d7421118c105e3bef213396212ece8552dda1e
Lindokuhle Hlatshwayo <lihlatsjhb024@student.wethinkcode.co.za>
2025-02-07 15:03:57 +0200

    Add initial implementation of functions and corresponding unit tests

---
+++ Python Katas/Kata 1/solutions.py
@@ -0,0 +1,35 @@
+# todo.py
+
+def add_two_numbers(a, b):
+    """
+    Add two numbers and return their sum.
+    """
+    pass
+
+
+def reverse_string(s):
+    """
+    Reverse a given string and return the reversed version.
+    """
+    pass
+
+
+def find_largest_number(numbers):
+    """
+    Find and return the largest number in a list of integers.
+    """
+    pass
+
+
+def count_vowels(s):
+    """
+    Count and return the number of vowels in a given string.
+    """
+    pass
+
+
+def fibonacci_sequence(n):
+    """
+    Generate and return the first n numbers in the Fibonacci sequence.
+    """
+    pass
