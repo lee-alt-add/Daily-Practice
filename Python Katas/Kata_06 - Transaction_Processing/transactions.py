918e5e4317341cdb572c3b243988469d4fee66d3
Lindokuhle Hlatshwayo <lindokuhlehlatshwayo@gmail.com>
2025-02-12 08:09:10 +0200

    Implement get_total_spent function to calculate total amount spent by a customer

--- Python Katas/Kata_06 - Transaction_Processing/transactions.py
+++ Python Katas/Kata_06 - Transaction_Processing/transactions.py
@@ -10,4 +10,15 @@ def add_transaction(transactions: dict, customer: str, transaction_id: str, amou
 
 def get_total_spent(transactions: dict, customer: str) -> int:
     """Returns total amount spent by a customer"""
+    total = 0
+    for customer_id, values in transactions.items():
+        if values['customer'] == customer:
+            total += values['amount']
     
+    return total
+# c_dict = {}
+
+# add_transaction(c_dict,"Alice", "T003", 50)
+# add_transaction(c_dict,"Alice", "T004", 70)
+# print(get_total_spent(c_dict, 'Alice'))
+            
