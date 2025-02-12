a6f1275c6620d481ed5d10b5bd6f408265415c50
Lindokuhle Hlatshwayo <lindokuhlehlatshwayo@gmail.com>
2025-02-12 08:05:46 +0200

    Add transaction validation and update unit test settings

--- Python Katas/Kata_06 - Transaction_Processing/transactions.py
+++ Python Katas/Kata_06 - Transaction_Processing/transactions.py
@@ -1,7 +1,13 @@
 
 def add_transaction(transactions: dict, customer: str, transaction_id: str, amount: int) -> str:
     """Adds a transaction"""
+    
+    if transaction_id in transactions.keys():
+        raise ValueError("Customer id already in use")
+    
+    transactions[transaction_id] = {"customer": customer, "amount": amount}
 
 
 def get_total_spent(transactions: dict, customer: str) -> int:
     """Returns total amount spent by a customer"""
+    
