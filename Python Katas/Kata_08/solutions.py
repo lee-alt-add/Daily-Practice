1f80b6b36ae04e624be763fe7e0fd8b967c8de49
Lindokuhle Hlatshwayo <lindokuhlehlatshwayo@gmail.com>
2025-02-19 16:38:44 +0200

    Completed group_anagrams function

--- Python Katas/Kata_08/solutions.py
+++ Python Katas/Kata_08/solutions.py
@@ -17,15 +17,28 @@ def longest_substring_without_repeating_chars(s):
     """
     Find the length of the longest substring without repeating characters.
     """
-    pass
+    # return sorted(list({k:s.count(k) for k in s}.items()))
 
 
 def group_anagrams(words):
     """
     Group a list of words into anagrams.
     """
-    pass
+    result = []
+    visited = set()
+    
+    for x in words:
+        holder = []
+        for y in words:
+            if sorted(x) == sorted(y) and y not in visited:
+                visited.add(y)
+                holder.append(y)
+        if holder:
+            result.append(holder)
 
+    return result
+
+print(group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
 
 def is_valid_parentheses(s):
     """
