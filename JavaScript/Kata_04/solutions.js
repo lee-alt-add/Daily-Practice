2071e3f8000b2c1a3671af41b4066acc59a27e3b
Lindokuhle Hlatshwayo <lindokuhlehlatshwayo@gmail.com>
2025-06-17 15:31:20 +0200

    Kata_04: Added calculateAverage function

--- JavaScript/Kata_04/solutions.js
+++ JavaScript/Kata_04/solutions.js
@@ -9,3 +9,12 @@ function addStudent(students, name, studentId, grade) {
     students[studentId] = new Map([[name, grade]]);
 }
 
+/**
+ * @param {Map} students
+ * @param {String} studentId
+ * @returns {Number}
+ */
+function calculateAverage(students, studentId) {
+    return students[studentId].reduce((a,b) => a+b, 0) / students[studentId].length;
+}
+
